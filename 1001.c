6fd482fc24896baaf464b69cc29c9c4e0d8eaaad
Ralf <ralfferreira995@gmail.com>
2022-10-04 21:08:20 -0300

    Add 1000, 1001, 1002 and README

---
+++ 1001.c
@@ -0,0 +1,16 @@
+/* 1001 - Extremamente Básico */
+
+#include <stdio.h>
+ 
+int main() { 
+    int A, B, X;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    X=A+B;
+
+    printf("X = %d\n",X);
+
+    return 0;
+}
---
+++ 1002.c
@@ -0,0 +1,15 @@
+/* 1002 - Área do Circulo */
+
+#include <stdio.h>
+ 
+int main(void) {
+ 
+    double area, raio;
+    
+    scanf("%lf", &raio);
+    area = 3.14159 * raio*raio;
+    
+    printf("A=%.4lf\n", area);
+ 
+    return 0;
+}
